d2dfd20c46498f90facbf2a71f54f01415a9874a
P Dring <pddring@googlemail.com>
2020-11-19 14:55:59 +0000

    Add project files.

---
+++ ChocolateBarDebugging/ChocolateBar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocolateBarDebugging
+{
+    class ChocolateBar
+    {
+        public string Name
+        public string Brand;
+        private int Calories;
+        
+        public ChocolateBar(string name)
+        {
+            Name = name;
+        }
+
+        public ChocolateBar()
+        {
+        }
+
+        public ChocolateBar(string name, string brand, int calories)
+        {
+            name = Name;
+            brand = Brand;
+            Calories = calories;
+        }
+
+        public void eat()
+        {
+            Console.WriteLine("Yum! You just ate a  + Brand + " " + Name);
+        }
+
+        public void giveTo(person)
+        {
+            Console.WriteLine("You just donated a chocolate bar");
+        }
+    }
+}
---
+++ ChocolateBarDebugging/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocolateBarDebugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChocolateBar boost = new ChocolateBar("Boost", "Cadbury", 250);
+            ChocolateBar twix = new ChocolateBar("Twix", "Mars", 286);
+
+            // program should output: "Yum! You just ate a Cadbury Boost"
+            boost.eat();
+
+            // program should output: "You just donated 286 calories of Mars goodness in Twix form to Mr Walsh"
+            twix.giveTo("Mr Walsh");
+
+            // Brand should be set to Mystery
+            // Name should be set to Chocolate Bar
+            ChocolateBar b = new ChocolateBar();
+            
+            // program should output: "Yum! You just ate a Mystery Chocolate Bar"
+            b.eat();
+        }
+    }
+}
